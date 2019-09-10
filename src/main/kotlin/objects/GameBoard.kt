9e24506ba28ac25654e5eb74ef7cc7e99be21b9d
unduli-attacked <45951442+unduli-attacked@users.noreply.github.com>
2019-09-09 18:44:35 -0700

    class setup/comments

---
+++ src/main/kotlin/objects/GameBoard.kt
@@ -0,0 +1,6 @@
+package objects
+
+public class GameBoard(){
+    // Put your code for the GameBoard() object here
+    // This could be things like questions, players, and points
+}
---
+++ src/main/kotlin/objects/Player.kt
@@ -0,0 +1,6 @@
+package objects;
+
+public class Player(){
+    // Put your code for the Player() object here
+    // This could be stuff like collected points or answered questions
+}
